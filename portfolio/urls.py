35508477fdcb4a9986a496296f77dd8cf0e0e6da
Anshul <samgalacestyle01@gmail.com>
2018-06-05 21:30:16 +0530

    Updated the url file with the 'working' static link

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,15 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+# from jobs import admin
 from django.urls import path
-from django.conf import settings
+# from django.conf import settings
+from portfolio import settings
 from django.conf.urls.static import static
-import os
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-] # + static('D\:Python\django\portfolio-project\media', document_root='/media/')
-
-#  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
